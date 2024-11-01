6f6fa503e17b2865cda08a1b254d21a314f998ea
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-11-01 13:19:13 +0300

    week one done

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your current location? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
